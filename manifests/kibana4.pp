cbe0a6fdc8023c54f516645c2cb9d61b82e65127
ronen <narkisr@gmail.com>
2016-01-17 02:24:13 +0200

    including global module and fixing source file

--- manifests/kibana4.pp
+++ manifests/kibana4.pp
@@ -1,18 +1,18 @@
 # Setting up kibana4
-class kibana4 {
+class elk::kibana4 {
 
-  include kibana4
+  include ::kibana4
 
   file { '/etc/systemd/system/kibana4.service':
-    ensure=> file,
-    mode  => '0644',
-    source=> 'puppet:///elk/module/kibana4.service',
-    owner => root,
-    group => root,
+    ensure => file,
+    mode   => '0644',
+    source => 'puppet:///modules/elk/kibana4.service',
+    owner  => root,
+    group  => root,
   } ->
   
-  exec { "systemd-daemon-reload":
-    command     => "/bin/systemctl daemon-reload",
+  exec { 'systemd-daemon-reload':
+    command     => '/bin/systemctl daemon-reload',
     refreshonly => true,
   } -> Service['kibana4']
 
