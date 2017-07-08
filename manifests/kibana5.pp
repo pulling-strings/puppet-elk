6ab29e2d0493b7cab22b9ff74f93095f57b8c0ef
ronen <narkisr@gmail.com>
2017-07-09 02:06:24 +1000

    fixing to match clean 5.x

--- manifests/kibana5.pp
+++ manifests/kibana5.pp
@@ -1,23 +1,11 @@
 # Setting up kibana4
 class elk::kibana5 {
-
-  package{'curl':
-    ensure  => present
+  class { 'kibana':
+    manage_repo => false,
+    config      => {
+      'server.port' => '8080',
+      'server.host' => '0.0.0.0'
+    }
   }
-
-  class {'::kibana4':}
-
-  file { '/etc/systemd/system/kibana4.service':
-    ensure => file,
-    mode   => '0644',
-    source => 'puppet:///modules/elk/kibana4.service',
-    owner  => root,
-    group  => root,
-  } ->
-  
-  exec { 'systemd-daemon-reload':
-    command     => '/bin/systemctl daemon-reload',
-    refreshonly => true,
-  } -> Service['kibana4']
 
 }
--- manifests/logstash.pp
+++ manifests/logstash.pp
@@ -1,13 +1,7 @@
 # Setting up logstash for streaming data into Kibana
-# Cluster is the internal elasticsearch output cluster name
 class elk::logstash {
-  class { 'logstash':
+  class { '::logstash':
     manage_repo      => false,
     service_provider => 'systemd'
   }
-
-  logstash::configfile { 'inputs':
-    source=> 'puppet:///modules/logconf/syslog.conf',
-  }
-
 }
